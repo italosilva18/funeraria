7fdaae4054314735ffeb08fa20e19d4b56ab454f
italosilva18 <italosilva14@hotmail.com>
2023-11-06 01:27:37 -0300

    Vamos refazer em py por conta de drivers.

--- db/models/plan.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Plan struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
-	// Adicione outros campos relevantes do plano
-}
